d7409db1e204ada154f20c4b346480bda4ed2bbe
Daniel Urbina <danielur1999@gmail.com>
2020-02-14 06:07:24 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,10 +1,10 @@
 # Your Code Here
 def map(array)
 empty_array = []
-i = 0 
-while i < array.length
-  empty_array << yield(array[i])
-  i += 1
+counter = 0 
+while counter < array.length
+  empty_array << yield(array[counter])
+  counter += 1
 end
  empty_array
 end 
@@ -12,14 +12,14 @@ end
 def reduce(array, starting_value = nil)
  if starting_value
    sum = starting_value
-   i = 0
+   counter = 0
  else
    sum = array[0]
-   i = 1
+   counter = 1
  end
- while i < array.length 
-  sum = yield(sum, array[i])
-  i += 1
+ while counter < array.length 
+  sum = yield(sum, array[counter])
+  counter += 1
  end
  sum
 end 
